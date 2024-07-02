2cf88d07871053b8d030be26360b777213e0b2c4
Iskander Ramos <iskanderramos8@gmail.com>
2024-07-02 09:47:50 -0600

    endpoints para registro de personas agregado

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 const app = express();
+
 
 import authRouter from './src/routes/auth.routes.js';
 import usuariosRouter from './src/routes/usuarios.routes.js';
 import kitsRouter from './src/routes/kits.routes.js'
 import registroRouter from './src/routes/registroPersonas.routes.js';
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 app.use('/auth', authRouter);
